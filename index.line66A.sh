e39edc6c1a6bb03e701229d8d48d0f04d18f3a31
hollygene <hmcqueary@uga.edu>
2017-08-29 18:08:47 -0400

    fix

--- index.line66A.sh
+++ index.line66A.sh
@@ -1,1 +1,1 @@
-/usr/local/samtools/latest/samtools index /panfs/pstor.storage/scratch/dwhlab/Holly/RNA_seq/fastq_files/Holly_GC_66_S12_R1_001_tophat_out/accepted_hits.66A.new.sorted.bam
+/usr/local/samtools/latest/samtools index /panfs/pstor.storage/scratch/dwhlab/Holly/RNA_seq/fastq_files/Holly_GC_66_S12_R1_001_tophat_outA./accepted_hits.66A.sorted.bam

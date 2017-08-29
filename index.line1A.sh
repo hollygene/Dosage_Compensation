944bd19fbccb9709d490bd217137df839c060264
hollygene <hmcqueary@uga.edu>
2017-08-29 13:09:28 -0400

    add more scripts

---
+++ index.line1A.sh
@@ -0,0 +1,1 @@
+/usr/local/samtools/latest/samtools index /panfs/pstor.storage/scratch/dwhlab/Holly/RNA_seq/fastq_files/Holly_GC_1_redo_R1_001_tophat_out/accepted_hits.1A.sorted.bam
---
+++ index.line1C.sh
@@ -0,0 +1,1 @@
+/usr/local/samtools/latest/samtools index /panfs/pstor.storage/scratch/dwhlab/Holly/RNA_seq/fastq_files/Holly_1C_S3_R1_001_tophat_out/accepted_hits.1C.sorted.bam

cfe69ab77305f1e9f50052075bec7bed128f1cfb
hollygene <hmcqueary@uga.edu>
2017-08-29 17:50:58 -0400

    fixed typo

--- index.line50a.sh
+++ index.line50a.sh
@@ -1,1 +1,1 @@
-/usr/local/samtools/latest/samtools index /panfs/pstor.storage/scratch/dwhlab/Holly/RNA_seq/fastq_files/Holly_MA_50_redo_S7_R1_001_tophat_out/accepted_hits.50a.sorted.bam
+/usr/local/samtools/latest/samtools index /panfs/pstor.storage/scratch/dwhlab/Holly/RNA_seq/fastq_files/Holly_MA_50_S7_R1_001_tophat_out/accepted_hits.50a.sorted.bam

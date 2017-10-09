161d13b6e4f5095693c636a9379acd17e541db8e
hollygene <hmcqueary@uga.edu>
2017-10-09 17:07:27 -0400

    fixed index all script to right directories

--- index_all.sh
+++ index_all.sh
@@ -1,7 +1,3 @@
-while read SampleName
-do
-  mkdir /panfs/pstor.storage/scratch/dwhlab/Holly/RNA_seq/fastq_files/INDEX${SampleName}
-
 #!/bin/bash
 
 #PBS -N j_s_samtools
@@ -10,10 +6,12 @@ do
 #PBS -l mem=100gb
 #PBS -l walltime=480:00:00
 
+while read SampleName
+do
+  mkdir /lustre1/hcm14449/SC_RNAseq/RNA_seq/tophat/INDEX${SampleName}
 cd $PBS_O_WORKDIR
 module load samtools/1.3.1
 time  samtools index \
-/panfs/pstor.storage/scratch/dwhlab/Holly/RNA_seq/fastq_files/${SampleName}/accepted_hits.bam
+/lustre1/hcm14449/SC_RNAseq/RNA_seq/tophat/${SampleName}/accepted_hits.bam
 
-done < /panfs/pstor.storage/scratch/dwhlab/Holly/RNA_seq/fastq_files/samples.txt
- 
+done < /lustre1/hcm14449/SC_RNAseq/RNA_seq/tophat/samples.txt

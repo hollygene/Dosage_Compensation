e99ae1a398401e6219c8bcf012ef59ce8c08e8b4
hollygene <hmcqueary@uga.edu>
2017-10-10 16:35:24 -0400

    changed some things inside the code so that hopefully it works this time

--- sort.all.sh
+++ sort.all.sh
@@ -5,12 +5,12 @@
 #PBS -l nodes=1:ppn=1:AMD
 #PBS -l mem=100gb
 #PBS -l walltime=480:00:00
-
+module load samtools/1.3.1
+cd /lustre1/hcm14449/SC_RNAseq/RNA_seq/tophat
 while read SampleName
 do
   mkdir /lustre1/hcm14449/SC_RNAseq/RNA_seq/tophat/SORT${SampleName}
-cd $PBS_O_WORKDIR
-module load samtools/1.3.1
+
 time  samtools sort \
 /lustre1/hcm14449/SC_RNAseq/RNA_seq/tophat/${SampleName}/accepted_hits.bam
 

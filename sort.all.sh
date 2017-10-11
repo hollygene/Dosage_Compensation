51e1c98de66e2de89a395e24656e643d6fc0fd4a
hollygene <hmcqueary@uga.edu>
2017-10-11 11:52:53 -0400

    typos

--- sort.all.sh
+++ sort.all.sh
@@ -5,16 +5,16 @@
 #PBS -l nodes=1:ppn=1:AMD
 #PBS -l mem=100gb
 #PBS -l walltime=480:00:00
+#PBS -M hmcqueary@uga.edu
+#PBS -m ae
 
 module load samtools/1.3.1
 cd /lustre1/hcm14449/SC_RNAseq/RNA_seq/tophat
 
 while read SampleName
 do
-  mkdir /lustre1/hcm14449/SC_RNAseq/RNA_seq/tophat/SORT${SampleName}
-
 time samtools sort -m 100G -o /lustre1/hcm14449/SC_RNAseq/RNA_seq/tophat/SORT${SampleName}/${SampleName}.sorted.bam \
--T /lustre1/hcm14449/SC_RNAseq/RNA_seq/tophat/SORT${SampleName}/accepted_hits \
-/lustre1/hcm14449/SC_RNAseq/RNA_seq/tophat/SORT${SampleName}/accepted_hits.bam
+-T /lustre1/hcm14449/SC_RNAseq/RNA_seq/tophat/${SampleName}/accepted_hits \
+/lustre1/hcm14449/SC_RNAseq/RNA_seq/tophat/${SampleName}/accepted_hits.bam
 
 done < /lustre1/hcm14449/SC_RNAseq/RNA_seq/tophat/samples.txt

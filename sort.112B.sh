a067554744a49a33ace7e8496d148b30638042b2
hollygene <hmcqueary@uga.edu>
2017-10-12 13:21:58 -0400

    new script to run sort on just line 112B

---
+++ sort.112B.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#PBS -N sort_112
+#PBS -q batch
+#PBS -l nodes=1:ppn=1:AMD
+#PBS -l mem=100gb
+#PBS -l walltime=480:00:00
+#PBS -M hmcqueary@uga.edu
+#PBS -m ae
+#PBS -o /lustre1/hcm14449/SC_RNAseq/sort_112.o
+#PBS -e /lustre1/hcm14449/SC_RNAseq/sort_112.e
+
+module load samtools/1.3.1
+cd /lustre1/hcm14449/SC_RNAseq/RNA_seq/tophat
+
+time samtools sort -m 100G -o /lustre1/hcm14449/SC_RNAseq/RNA_seq/tophat/SORTHolly_112B_S28_R1_001_tophat_out/Holly_112B_S28_R1_001_tophat_out.sorted.bam \
+-T /lustre1/hcm14449/SC_RNAseq/RNA_seq/tophat/Holly_112B_S28_R1_001_tophat_out/accepted_hits \
+/lustre1/hcm14449/SC_RNAseq/RNA_seq/tophat/Holly_112B_S28_R1_001_tophat_out/accepted_hits.bam

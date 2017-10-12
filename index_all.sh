99586a32925357c2af59ff30353489591d2cf805
hollygene <hmcqueary@uga.edu>
2017-10-11 20:21:49 -0400

    edgeR changes also made code to do index on all lines

--- index_all.sh
+++ index_all.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-#PBS -N j_s_samtools
+#PBS -N sort_all
 #PBS -q batch
 #PBS -l nodes=1:ppn=1:AMD
 #PBS -l mem=100gb
 #PBS -l walltime=480:00:00
+#PBS -M hmcqueary@uga.edu
+#PBS -m ae
+
+module load samtools/1.3.1
+cd /lustre1/hcm14449/SC_RNAseq/RNA_seq/tophat
 
 while read SampleName
 do
-  mkdir /lustre1/hcm14449/SC_RNAseq/RNA_seq/tophat/INDEX${SampleName}
-cd $PBS_O_WORKDIR
-module load samtools/1.3.1
 time  samtools index \
-/lustre1/hcm14449/SC_RNAseq/RNA_seq/tophat/${SampleName}/accepted_hits.bam
+/lustre1/hcm14449/SC_RNAseq/RNA_seq/tophat/SORT${SampleName}/${SampleName}.sorted.bam
 
 done < /lustre1/hcm14449/SC_RNAseq/RNA_seq/tophat/samples.txt

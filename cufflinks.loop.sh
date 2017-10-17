1976a849ad3add66e9ee10af41ebbae8a2d517f9
hollygene <hmcqueary@uga.edu>
2017-10-17 12:16:02 -0400

    cufflinks loop and samples file to run loop

---
+++ cufflinks.loop.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#PBS -q batch
+#PBS -l nodes=1:ppn=4:AMD
+#PBS -l walltime=96:00:00
+#PBS -l pmem=20gb
+#PBS -M hmcqueary@uga.edu
+#PBS -m ae
+
+mkdir /lustre1/hcm14449/SC_RNAseq/RNA_seq/MA_old/Cufflinks
+cd /lustre1/hcm14449/SC_RNAseq/RNA_seq/MA_old/Cufflinks
+
+module load cufflinks/2.2.1
+
+while read SampleName
+do
+mkdir /panfs/pstor.storage/scratch/dwhlab/Holly/RNA_seq/CufflinksOutput/cufflinks${SampleName}
+
+time cufflinks -p 4
+-g /lustre1/hcm14449/SC_RNAseq/RNA_seq/tophat/genes.gtf \
+-o /panfs/pstor.storage/scratch/dwhlab/Holly/RNA_seq/CufflinksOutput/cufflinks${SampleName} \
+-b /lustre1/hcm14449/SC_RNAseq/RNA_seq/tophatgenome.fa \
+/lustre1/hcm14449/SC_RNAseq/RNA_seq/MA_old/tophat${SampleName}/accepted_hits.bam
+
+done < /home/hcm14449/Github/Dosage_Compensation/MAsamples.txt

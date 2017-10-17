765ab6f7e90c475461b2b2d01999b6c6ca845dae
hollygene <hmcqueary@uga.edu>
2017-10-17 11:54:32 -0400

    running this script on two files that were missing from the loop

--- test_one_file_tophat.sh
+++ test_one_file_tophat.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#PBS -N test.sample1
+#PBS -N sample6_1
 #PBS -q batch
 #PBS -l nodes=1:ppn=4:AMD
 #PBS -l walltime=96:00:00
@@ -7,8 +7,17 @@
 #PBS -M hmcqueary@uga.edu
 #PBS -m ae
 
-cd /lustre1/hcm14449/SC_RNAseq/RNA_seq/tophat
+cd /lustre1/hcm14449/SC_RNAseq/RNA_seq/MA_old/fastq
 module load tophat/2.1.1
 
-time  tophat -p 4 -o ./SC001_1_raw.fastq_tophat_out -i 10 -I 1000 \
---transcriptome-index=transcriptome_data/known genome SC001_1_raw.fastq
+time  tophat -p 4 -o /lustre1/hcm14449/SC_RNAseq/RNA_seq/MA_old/tophat/SC006_1_raw_tophat_out \
+-i 10 -I 1000 \
+--transcriptome-index=/lustre1/hcm14449/SC_RNAseq/RNA_seq/tophat/transcriptome_data/known \
+/lustre1/hcm14449/SC_RNAseq/RNA_seq/tophat/genome \
+./SC006_1_raw.fastq
+
+time  tophat -p 4 -o /lustre1/hcm14449/SC_RNAseq/RNA_seq/MA_old/tophat/SCA_3_raw_tophat_out \
+-i 10 -I 1000 \
+--transcriptome-index=/lustre1/hcm14449/SC_RNAseq/RNA_seq/tophat/transcriptome_data/known \
+/lustre1/hcm14449/SC_RNAseq/RNA_seq/tophat/genome \
+./SCA_3_raw.fastq

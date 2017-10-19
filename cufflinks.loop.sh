35b6d164dd1316a9ca9e46bd52e06ab39a97cb56
hollygene <hmcqueary@uga.edu>
2017-10-19 12:57:44 -0400

    asked for less gb and threads

--- cufflinks.loop.sh
+++ cufflinks.loop.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #PBS -q batch
-#PBS -l nodes=1:ppn=4:AMD
+#PBS -l nodes=1:ppn=1:AMD
 #PBS -l walltime=96:00:00
-#PBS -l pmem=20gb
+#PBS -l pmem=5gb
 #PBS -M hmcqueary@uga.edu
 #PBS -m ae
 

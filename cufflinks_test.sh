beb5a4477b502531d333e8c71985bae105cb5533
hollygene <hmcqueary@uga.edu>
2017-10-19 13:14:25 -0400

    running cufflinks on one file to test my code

---
+++ cufflinks_test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#PBS -q batch
+#PBS -l nodes=1:ppn=1:AMD
+#PBS -l walltime=96:00:00
+#PBS -l pmem=5gb
+#PBS -M hmcqueary@uga.edu
+#PBS -m ae
+
+cd /lustre1/hcm14449/SC_RNAseq/RNA_seq/MA_old/Cufflinks
+
+module load cufflinks/2.2.1
+
+mkdir /lustre1/hcm14449/SC_RNAseq/RNA_seq/MA_old/Cufflinks/cufflinksSC001_1_raw.fastq_tophat_out
+
+time cufflinks
+-g /lustre1/hcm14449/SC_RNAseq/RNA_seq/tophat/genes.gtf \
+-o /lustre1/hcm14449/SC_RNAseq/RNA_seq/MA_old/Cufflinks/cufflinksSC001_1_raw.fastq_tophat_out \
+-b /lustre1/hcm14449/SC_RNAseq/RNA_seq/tophatgenome.fa \
+/lustre1/hcm14449/SC_RNAseq/RNA_seq/MA_old/tophatSC001_1_raw.fastq_tophat_out/accepted_hits.bam

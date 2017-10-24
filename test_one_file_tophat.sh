ad10bffe00ff7a27e0df65bb6b8e1d5c83965cfb
hollygene <hmcqueary@uga.edu>
2017-10-24 14:52:43 -0400

    changed location of the fastq file to new one

--- test_one_file_tophat.sh
+++ test_one_file_tophat.sh
@@ -7,10 +7,10 @@
 #PBS -M hmcqueary@uga.edu
 #PBS -m ae
 
-cd /lustre1/hcm14449/SC_RNAseq/RNA_seq/MA_old/fastq
+cd /lustre1/hcm14449/SC_RNAseq/RNA_seq/MA_old/tophat
 module load tophat/2.1.1
 
-time  tophat -p 4 -o /lustre1/hcm14449/SC_RNAseq/RNA_seq/MA_old/tophat/SC006_1_raw_tophat_out \
+time  tophat -p 4 -o /lustre1/hcm14449/SC_RNAseq/RNA_seq/MA_old/tophat/SC006_1_raw_tophat_out1 \
 -i 10 -I 1000 \
 --transcriptome-index=/lustre1/hcm14449/SC_RNAseq/RNA_seq/tophat/transcriptome_data/known \
 /lustre1/hcm14449/SC_RNAseq/RNA_seq/tophat/genome \

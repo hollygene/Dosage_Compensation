126a943da03a4b3097f7501a1b9c11d7742ad0e4
hollygene <hmcqueary@uga.edu>
2017-11-17 13:19:00 -0500

    trying tophat in a loop instead of bowtie. Bowtie takes a lot more steps and takes longer

--- assembly_script.sh
+++ assembly_script.sh
@@ -40,98 +40,78 @@ cd /lustre1/hcm14449/SC_RNAseq/RNA_seq/November_2017_Assembly/GC
 #module unload trimgalore/0.4.4
 
 #map to reference using bowtie2
-
 #module load bowtie2/2.2.9
-
 #build index for ref genome
 #cd /lustre1/hcm14449/SC_RNAseq/RNA_seq/November_2017_Assembly/
 #bowtie2-build genome.fa genome
-
 #bowtie2-inspect -s genome
-
 #map sequences for line 2 first
 cd /lustre1/hcm14449/SC_RNAseq/RNA_seq/November_2017_Assembly/GC/trimmed
 #bowtie2 -x /lustre1/hcm14449/SC_RNAseq/RNA_seq/November_2017_Assembly/genome -U Holly_2A_S1_R1_001_trimmed.fq -S Holly_2A.sam
 #bowtie2 -x /lustre1/hcm14449/SC_RNAseq/RNA_seq/November_2017_Assembly/genome -U Holly_2B_S7_R1_001_trimmed.fq -S Holly_2B.sam
 #bowtie2 -x /lustre1/hcm14449/SC_RNAseq/RNA_seq/November_2017_Assembly/genome -U Holly_2C_S4_R1_001_trimmed.fq -S Holly_2C.sam
-
 #bowtie2 -x /lustre1/hcm14449/SC_RNAseq/RNA_seq/November_2017_Assembly/genome -U Holly-GC-7-redo_S4_R1_001_trimmed.fq -S Holly_7A.sam
 #bowtie2 -x /lustre1/hcm14449/SC_RNAseq/RNA_seq/November_2017_Assembly/genome -U Holly_7B_S12_R1_001_trimmed.fq -S Holly_7B.sam
 #bowtie2 -x /lustre1/hcm14449/SC_RNAseq/RNA_seq/November_2017_Assembly/genome -U Holly_7C_S9_R1_001_trimmed.fq -S Holly_7C.sam
-
 #for file in ./*.fq
-
 #do
-
 #FBASE=$(basename $file .fq)
 #BASE=${FBASE%.fq}
-
 #bowtie2 -x /lustre1/hcm14449/SC_RNAseq/RNA_seq/November_2017_Assembly/genome \
 #-U ${BASE}.fq -S ${BASE}.sam
-
 #done
-
 #module unload bowtie2/2.2.9
-
-module load samtools/1.3.1
+#module load samtools/1.3.1
 #turn sam files into bam files
-
-samtools view -b Holly_2A_S1_R1_001_trimmed.sam -o Holly_2A_S1_R1_001_trimmed.bam
-samtools view -b Holly_2B_S7_R1_001_trimmed.sam -o Holly_2B_S7_R1_001_trimmed.bam
-samtools view -b Holly_2C_S4_R1_001_trimmed.sam -o Holly_2C_S4_R1_001_trimmed.bam
-
+#samtools view -bS -T  Holly_2A_S1_R1_001_trimmed.sam -o Holly_2A_S1_R1_001_trimmed.bam
+#samtools view -bS Holly_2B_S7_R1_001_trimmed.sam -o Holly_2B_S7_R1_001_trimmed.bam
+#samtools view -bS Holly_2C_S4_R1_001_trimmed.sam -o Holly_2C_S4_R1_001_trimmed.bam
 #samtools view -b Holly_7B_S12_R1_001_trimmed.sam
 #samtools view -b Holly_7C_S9_R1_001_trimmed.sam
 #samtools view -b Holly-A-7-redo_S4_R1_001_trimmed.sam
-
 #sort the bam files
 #samtools sort -m 5G -@ $THREADS -O bam -T Holly_2A.tmp Holly_2A.bam > Holly_2A.sort.bam
 #samtools sort -m 5G -@ $THREADS -O bam -T Holly_2B.tmp Holly_2B.bam > Holly_2B.sort.bam
 #samtools sort -m 5G -@ $THREADS -O bam -T Holly_2C.tmp Holly_2C.bam > Holly_2C.sort.bam
-
 #samtools sort -m 5G -@ $THREADS -O bam -T Holly_7A.tmp Holly_7A.bam > Holly_7A.sort.bam
 #samtools sort -m 5G -@ $THREADS -O bam -T Holly_7B.tmp Holly_7B.bam > Holly_7B.sort.bam
 #samtools sort -m 5G -@ $THREADS -O bam -T Holly_7C.tmp Holly_7C.bam > Holly_7C.sort.bam
-
 #module unload samtools/1.3.1
 #load in bwa for mapping
 #module load bwa/0.7.15
-
 #bwa index genome.fa
-
 #map reads to ref genome using bwa
 #bwa mem Holly_2A_S1_R1_001.fastq
-
 #Holly_2B_S7_R1_001.fastq
 #Holly_2C_S4_R1_001.fastq
 
 #load in tophat
-#module load tophat/2.1.1
+module load tophat/2.1.1
 
 #index transcriptome file
-#tophat -G genes.gtf --transcriptome-index=transcriptome_data/known genome
+tophat -G genes.gtf --transcriptome-index=transcriptome_data/known genome
 
 #run tophat on all samples
 #do GC ones first
-#cd /lustre1/hcm14449/SC_RNAseq/RNA_seq/November_2017_Assembly/GC
-#mkdir "/lustre1/hcm14449/SC_RNAseq/RNA_seq/November_2017_Assembly/GC/tophat_test/"
+cd /lustre1/hcm14449/SC_RNAseq/RNA_seq/November_2017_Assembly/GC/trimmed
+mkdir "/lustre1/hcm14449/SC_RNAseq/RNA_seq/November_2017_Assembly/GC/trimmed/tophat_test/"
 
-#for file in ./*.fastq
+for file in ./*.fq
 
-#do
+do
 
-#FBASE=$(basename $file .fastq)
-#BASE=${FBASE%.fastq}
-#tophat -p $THREADS -o /lustre1/hcm14449/SC_RNAseq/RNA_seq/November_2017_Assembly/GC/tophat_test/${BASE}_tophat_out \
-#-i 10 -I 1000 \
-#--transcriptome-index=/lustre1/hcm14449/SC_RNAseq/RNA_seq/November_2017_Assembly/transcriptome_data/known \
-#/lustre1/hcm14449/SC_RNAseq/RNA_seq/November_2017_Assembly/genome \
-#./${BASE}.fastq
+FBASE=$(basename $file .fq)
+BASE=${FBASE%.fq}
+tophat -p $THREADS -o /lustre1/hcm14449/SC_RNAseq/RNA_seq/November_2017_Assembly/GC/trimmed/tophat_test/${BASE}_tophat_out \
+-i 10 -I 1000 \
+--transcriptome-index=/lustre1/hcm14449/SC_RNAseq/RNA_seq/November_2017_Assembly/transcriptome_data/known \
+/lustre1/hcm14449/SC_RNAseq/RNA_seq/November_2017_Assembly/genome \
+./${BASE}.fq
 
-#done
+done
 
 #unload tophat
-#module unload tophat/2.1.1
+module unload tophat/2.1.1
 
 #load in cufflinks
 #module load cufflinks/2.2.1

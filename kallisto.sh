cab32f8e211785ee442c5564b1edb9e83935b497
hollygene <hmcqueary@uga.edu>
2017-12-12 11:23:56 -0500

    indexing transcriptome worked, now testing it on one line

--- kallisto.sh
+++ kallisto.sh
@@ -13,12 +13,12 @@ THREADS=4
 
 module load kallisto/0.42.5
 #first need to build a kallisto index
-wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/146/045/GCF_000146045.2_R64/GCF_000146045.2_R64_rna.fna.gz
+#wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/146/045/GCF_000146045.2_R64/GCF_000146045.2_R64_rna.fna.gz
 
-/usr/local/apps/kallisto/0.42.5/bin/kallisto index -i transcripts.idx GCF_000146045.2_R64_rna.fna.gz
+#/usr/local/apps/kallisto/0.42.5/bin/kallisto index -i transcripts.idx GCF_000146045.2_R64_rna.fna.gz
 
 #test it on one line first
 
-#cd /lustre1/hcm14449/SC_RNAseq/RNA_seq/November_2017_Assembly/GC
+cd /lustre1/hcm14449/SC_RNAseq/RNA_seq/November_2017_Assembly/GC
 
-#/usr/local/apps/kallisto/0.42.5/bin/kallisto quant -i genome -o test.kallisto --single -l 200 -s 20 Holly-66B-redo-136232_S14_R1_001.fastq
+/usr/local/apps/kallisto/0.42.5/bin/kallisto quant -i genome -o test.kallisto --single -l 200 -s 20 Holly-66B-redo-136232_S14_R1_001.fastq

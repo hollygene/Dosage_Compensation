80afa6d5148c2858aec4ba03a6449f0c771501d0
hollygene <hmcqueary@uga.edu>
2017-12-12 10:33:32 -0500

    testing kallisto: building index and trying it on one line

---
+++ kallisto.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#PBS -N assembly_test_2
+#PBS -q batch
+#PBS -l nodes=1:ppn=1:HIGHMEM
+#PBS -l walltime=480:00:00
+#PBS -l mem=100gb
+#PBS -M hmcqueary@uga.edu
+#PBS -m ae
+
+basedir="/lustre1/hcm14449/SC_RNAseq/RNA_seq/November_2017_Assembly"
+cd $basedir
+THREADS=4
+
+module load kallisto/0.42.5
+#first need to build a kallisto index
+/usr/local/apps/kallisto/0.42.5/bin/kallisto index -i genome genome.fa
+
+#test it on one line first
+cd /lustre1/hcm14449/SC_RNAseq/RNA_seq/November_2017_Assembly/GC
+
+/usr/local/apps/kallisto/0.42.5/bin/kallisto quant -i genome -o test.kallisto --single -l 200 -s 20 Holly-66B-redo-136232_S14_R1_001.fastq

598ec1b2f446f6bf760315cafb6cfa70ff8450c4
hollygene <hmcqueary@uga.edu>
2018-02-16 09:57:09 -0500

    needed to ask for more walltime

--- htseq.sh
+++ htseq.sh
@@ -2,17 +2,19 @@
 #PBS -q batch
 #PBS -N htseq
 #PBS -l nodes=1:ppn=1:HIGHMEM
-#PBS -l walltime=4:00:00
+#PBS -l walltime=48:00:00
 #PBS -l mem=200gb
 
-cd $PBS_O_WORKDIR
+basedir="/lustre1/hcm14449/SC_RNAseq/RNA_seq/November_2017_Assembly"
+cd $basedir
+mkdir /lustre1/hcm14449/SC_RNAseq/RNA_seq/November_2017_Assembly/GC/HTseq/
 
 module load python/2.7.8
 module load htseq/0.6.1p1
 
 while read SampleName
 do
-mkdir /lustre1/hcm14449/SC_RNAseq/RNA_seq/November_2017_Assembly/GC/HTseq/
+
 
 htseq-count \
   -f bam \

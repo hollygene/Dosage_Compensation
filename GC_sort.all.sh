a98a63dbdf0f0ac7fb222af2849fbfeebfb7fad3
hollygene <hmcqueary@uga.edu>
2018-02-21 14:32:12 -0500

    sort sam files before putting into htseq

---
+++ GC_sort.all.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#PBS -N sort_all
+#PBS -q batch
+#PBS -l nodes=1:ppn=1:AMD
+#PBS -l mem=100gb
+#PBS -l walltime=480:00:00
+#PBS -M hmcqueary@uga.edu
+#PBS -m ae
+#PBS -o /lustre1/hcm14449/SC_RNAseq/sort_all.o
+#PBS -e /lustre1/hcm14449/SC_RNAseq/sort_all.e
+
+module load samtools/1.3.1
+cd /lustre1/hcm14449/SC_RNAseq/RNA_seq/November_2017_Assembly/GC/trimmed/tophat_test
+
+while read SampleName
+do
+time samtools sort -m 100G -o /lustre1/hcm14449/SC_RNAseq/RNA_seq/November_2017_Assembly/GC/trimmed/tophat_test/SORT${SampleName}/${SampleName}.sorted.bam \
+-T /lustre1/hcm14449/SC_RNAseq/RNA_seq/November_2017_Assembly/GC/trimmed/tophat_test/${SampleName}/accepted_hits \
+/lustre1/hcm14449/SC_RNAseq/RNA_seq/November_2017_Assembly/GC/trimmed/tophat_test/${SampleName}/accepted_hits.bam
+
+done < /lustre1/hcm14449/SC_RNAseq/RNA_seq/November_2017_Assembly/GC/trimmed/tophat_test/output.txt

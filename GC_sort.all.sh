c02ab1b6dc663cd78787822d797c1aab3192b3c6
hollygene <hmcqueary@uga.edu>
2018-02-22 10:54:49 -0500

    trying to get rid of -T command and see if it works. also asked for one thread instead of4

--- GC_sort.all.sh
+++ GC_sort.all.sh
@@ -2,7 +2,7 @@
 
 #PBS -N sort_all
 #PBS -q batch
-#PBS -l nodes=1:ppn=4:HIGHMEM
+#PBS -l nodes=1:ppn=1:HIGHMEM
 #PBS -l mem=200gb
 #PBS -l walltime=480:00:00
 #PBS -M hmcqueary@uga.edu
@@ -15,8 +15,8 @@ cd /lustre1/hcm14449/SC_RNAseq/RNA_seq/November_2017_Assembly/GC/trimmed/tophat_
 
 while read SampleName
 do
-time samtools sort -m 100G -o /lustre1/hcm14449/SC_RNAseq/RNA_seq/November_2017_Assembly/GC/trimmed/tophat_test/SORT${SampleName}/${SampleName}.sorted.bam \
--T /lustre1/hcm14449/SC_RNAseq/RNA_seq/November_2017_Assembly/GC/trimmed/tophat_test/${SampleName}/accepted_hits \
-/lustre1/hcm14449/SC_RNAseq/RNA_seq/November_2017_Assembly/GC/trimmed/tophat_test/${SampleName}/accepted_hits.bam
+time samtools sort -m 100G \
+-o /lustre1/hcm14449/SC_RNAseq/RNA_seq/November_2017_Assembly/GC/trimmed/tophat_test/SORT${SampleName}/${SampleName}.sorted.bam \
+   /lustre1/hcm14449/SC_RNAseq/RNA_seq/November_2017_Assembly/GC/trimmed/tophat_test/${SampleName}/accepted_hits.bam
 
 done < /lustre1/hcm14449/SC_RNAseq/RNA_seq/November_2017_Assembly/GC/trimmed/tophat_test/output.txt

7158f8390690c674e6328c712e168a676cc94e1b
hollygene <hmcqueary@uga.edu>
2018-02-26 14:46:57 -0500

    make directory, -O bam, made sure file existed...

--- sort.112B.sh
+++ sort.112B.sh
@@ -1,16 +1,18 @@
-#!/bin/bash
-
-#PBS -N sort_112B
+#PBS -S /bin/bash
 #PBS -q batch
+#PBS -N samtools_sort
 #PBS -l nodes=1:ppn=1:HIGHMEM
-#PBS -l mem=200gb
 #PBS -l walltime=480:00:00
-#PBS -M hmcqueary@uga.edu
-#PBS -m ae
+#PBS -l mem=200gb
+
 
 module load samtools/1.3.1
 cd /lustre1/hcm14449/SC_RNAseq/RNA_seq/tophat
 
-time samtools sort -m 200G -o /lustre1/hcm14449/SC_RNAseq/RNA_seq/tophat/SORTHolly_112B_S28_R1_001_trimmed_tophat_out/Holly_112B_S28_R1_001_trimmed_tophat_out.sorted.bam \
--T /lustre1/hcm14449/SC_RNAseq/RNA_seq/November_2017_Assembly/MA_new/trimmed/tophat/Holly_112B_S28_R1_001_trimmed_tophat_out/accepted_hits \
+mkdir /lustre1/hcm14449/SC_RNAseq/RNA_seq/tophat/SORTHolly_112B_S28_R1_001_trimmed_tophat_out
+
+time samtools sort \
+-m 200G \
+-o /lustre1/hcm14449/SC_RNAseq/RNA_seq/tophat/SORTHolly_112B_S28_R1_001_trimmed_tophat_out/Holly_112B_S28_R1_001_trimmed_tophat_out.sorted.bam \
+-O bam \
 /lustre1/hcm14449/SC_RNAseq/RNA_seq/November_2017_Assembly/MA_new/trimmed/tophat/Holly_112B_S28_R1_001_trimmed_tophat_out/accepted_hits.bam

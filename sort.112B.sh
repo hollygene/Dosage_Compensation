f18c593c008b488ae58e7126bd737493f3a9ef46
hollygene <hmcqueary@uga.edu>
2018-02-26 14:28:34 -0500

    had the wrong file name

--- sort.112B.sh
+++ sort.112B.sh
@@ -11,6 +11,6 @@
 module load samtools/1.3.1
 cd /lustre1/hcm14449/SC_RNAseq/RNA_seq/tophat
 
-time samtools sort -m 200G -o /lustre1/hcm14449/SC_RNAseq/RNA_seq/tophat/SORTHolly_112B_S28_R1_001_tophat_out/Holly_112B_S28_R1_001_tophat_out.sorted.bam \
--T /lustre1/hcm14449/SC_RNAseq/RNA_seq/November_2017_Assembly/MA_new/trimmed/tophat/Holly_112B_S28_R1_001_tophat_out/accepted_hits \
-/lustre1/hcm14449/SC_RNAseq/RNA_seq/November_2017_Assembly/MA_new/trimmed/tophat/Holly_112B_S28_R1_001_tophat_out/accepted_hits.bam
+time samtools sort -m 200G -o /lustre1/hcm14449/SC_RNAseq/RNA_seq/tophat/SORTHolly_112B_S28_R1_001_trimmed_tophat_out/Holly_112B_S28_R1_001_trimmed_tophat_out.sorted.bam \
+-T /lustre1/hcm14449/SC_RNAseq/RNA_seq/November_2017_Assembly/MA_new/trimmed/tophat/Holly_112B_S28_R1_001_trimmed_tophat_out/accepted_hits \
+/lustre1/hcm14449/SC_RNAseq/RNA_seq/November_2017_Assembly/MA_new/trimmed/tophat/Holly_112B_S28_R1_001_trimmed_tophat_out/accepted_hits.bam

f0224b9e1d799efb08f648d914ace51fa7230c4b
hollygene <hmcqueary@uga.edu>
2018-03-27 11:38:01 -0400

    highmem node

--- tophat_50c.sh
+++ tophat_50c.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #PBS -N sample1_3
 #PBS -q batch
-#PBS -l nodes=1:ppn=4:AMD
+#PBS -l nodes=1:ppn=4:HIGHMEM
 #PBS -l walltime=96:00:00
-#PBS -l pmem=20gb
+#PBS -l pmem=200gb
 #PBS -M hmcqueary@uga.edu
 #PBS -m ae
 

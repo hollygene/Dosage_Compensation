307b13df291643f08957bc71d6b0fb3b7838fcca
hollygene <hmcqueary@uga.edu>
2019-02-28 15:46:31 -0500

    trying again with things in different places

--- script_testing_2.28.19.sh
+++ script_testing_2.28.19.sh
@@ -7,15 +7,15 @@
 #PBS -M hmcqueary@uga.edu
 #PBS -m ae
 
-basedir="/project/dwhlab/Holly/SC_RNAseq/RNA_seq/November_2017_Assembly/GC/fastqsGC"
+basedir="/scratch/hcm14449/DC_Feb2019/GC/fastqsGC"
 THREADS=4
 fastqc_module="FastQC/0.11.8-Java-1.8.0_144"
 trimgalore_module="Trim_Galore/0.4.5-foss-2016b"
 ######### GC LINES ##############
-GC_data_dir="/project/dwhlab/Holly/SC_RNAseq/RNA_seq/November_2017_Assembly/GC/fastqsGC"
-mkdir /scratch/hcm14449/DC_Feb2019/GC/fastqsGC/fastQC
+GC_data_dir="/scratch/hcm14449/DC_Feb2019/GC/fastqsGC"
+# mkdir /scratch/hcm14449/DC_Feb2019/GC/fastqsGC/fastQC
 GC_fastQC_dir= "/scratch/hcm14449/DC_Feb2019/GC/fastqsGC/fastQC"
-ref_genome="/project/dwhlab/Holly/SC_RNAseq/RNA_seq/November_2017_Assembly/SCerRefGenome/genome.fa"
+# ref_genome="/project/dwhlab/Holly/SC_RNAseq/RNA_seq/November_2017_Assembly/SCerRefGenome/genome.fa"
 GC_trimmed_dir="/scratch/hcm14449/DC_Feb2019/GC/trim"
 
 #run FASTQC on samples to make sure things look good and I don't need to remove any adapters or anything
@@ -45,7 +45,7 @@ do
 FBASE=$(basename $file .fastq)
 BASE=${FBASE%.fastq}
 
-fastqc $GC_data_dir/${BASE}.fastq -o ${GC_fastQC_dir}
+fastqc $GC_data_dir/${BASE}.fastq -o $GC_fastQC_dir
 
 done
 

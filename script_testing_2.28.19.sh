7bcc93f6d6704b64d312cd65d6199b993619b250
hollygene <hmcqueary@uga.edu>
2019-02-28 13:55:20 -0500

    trying again

--- script_testing_2.28.19.sh
+++ script_testing_2.28.19.sh
@@ -12,7 +12,8 @@ THREADS=4
 fastqc_module="FastQC/0.11.8-Java-1.8.0_144"
 trimgalore_module="Trim_Galore/0.4.5-foss-2016b"
 ######### GC LINES ##############
-GC_data_dir="/project/dwhlab/Holly/SC_RNAseq/RNA_seq/November_2017_Assembly/GC/fastqsGC"
+GC_data_dir="/project/dwhlab/Holly/SC_RNAseq/RNA_seq/November_2017_Assembly/GC/fastqsGC/"
+mkdir /scratch/hcm14449/DC_Feb2019/GC/fastqsGC/fastQC
 GC_fastQC_dir= "/scratch/hcm14449/DC_Feb2019/GC/fastqsGC/fastQC"
 ref_genome="/project/dwhlab/Holly/SC_RNAseq/RNA_seq/November_2017_Assembly/SCerRefGenome/genome.fa"
 GC_trimmed_dir="/scratch/hcm14449/DC_Feb2019/GC/trim"
@@ -21,11 +22,10 @@ GC_trimmed_dir="/scratch/hcm14449/DC_Feb2019/GC/trim"
 
 # trying to rename files so that I can eventually run a loop with cuffdiff
 # rename 'GC' A *.fq
-mkdir ${GC_fastQC_dir}
 
 module load ${fastqc_module}
 
-for file in ${GC_data_dir}/*.fastq
+for file in ${GC_data_dir}*.fastq
 
 do
 
